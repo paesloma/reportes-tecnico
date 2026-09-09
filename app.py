1ccd1be9718be6f0344a2b08b392fab3cc216df1
paesloma <58999041+paesloma@users.noreply.github.com>
2026-09-09 14:27:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ def generar_txt_contenido(datos, secciones_activas, idioma):
     return txt
 
 # --- 5. INTERFAZ ---
-st.title("🚀 Gestión de Reportes Técnicos")
+st.title("REPORTES TECNICOS RECLAMO AL PROVEEDOR")
 
 orden_id = st.text_input("Ingrese número de Orden")
 c_v, s_v, p_v, f_v, ff_v = "", "", "", "", date.today()
